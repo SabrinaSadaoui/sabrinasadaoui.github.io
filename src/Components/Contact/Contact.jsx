df0df35aacb41b8f9b5f2ca1626a31112d1c0c81
SabrinaSadaoui <sabsada28@gmail.com>
2023-05-18 22:14:23 +0200

    json update navbar + mail icon

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Contact.css";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Contact() {
@@ -9,7 +10,7 @@ export default function Contact() {
       <h2>Interressée par mon profile ?</h2>
       <h3>Contactez moi :</h3>
       <a className="contactLink" href="sadaoui.sabrina@hotmail.com">
-        <FontAwesomeIcon className="contactIcon" icon={faLinkedin} size="1x" />
+        <FontAwesomeIcon className="contactIcon" icon={faEnvelope} size="1x" />
         <p>Par Mail</p>
       </a>
       <a className="contactLink" href="https://www.linkedin.com/in/sabrina-sadaoui-38bb18129/">
