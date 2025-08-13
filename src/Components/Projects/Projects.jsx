d03f05484b3fb2ecde050de756c8bc3685da5914
SabrinaSadaoui <sabsada28@gmail.com>
2025-08-13 17:23:27 +0200

    Update Projects.jsx

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -5,6 +5,7 @@ import hip from "/projects/Hack_In_Paris.png";
 import kntd from "/projects/Logo_Kool_N_The_Dance.png";
 import hubvous from "/projects/Logo_Hub&Vous.png";
 import hubsite from "/projects/Hub_One.png";
+import tagheuer from "/projects/TAG_Heuer_webmastering.png";
 import sysdream from "/projects/SysDream.png";
 import bootstrap from "/tools/dev/bootstrap.png";
 import html5 from "/tools/dev/html5.png";
@@ -15,25 +16,36 @@ import jquery from "/tools/dev/jquery.png";
 import adobeXD from "/tools/design/adobexd.png";
 import vscode from "/tools/dev/vscode.png";
 import illustrator from "/tools/design/illustrator.png"
+import photoshop from "/tools/design/photoshop.png"
+import jira from "/tools/management/jira.png";
+import salesforce from "/tools/management/Salesforce.com_logo.svg.png";
+import figma from "/tools/design/figma.png";
 
 export default function Projects() {
   const devList = [
     {
       id: "1",
+      projectName: "TAG Heuer",
+      lienProject: "www.tagheuer.com",
+      imgP: tagheuer,
+      skillName: [salesforce, jira, photoshop, figma, html5, css3, vscode],
+    },
+    {
+      id: "2",
       projectName: "Hub One",
       lienProject: "www.hubone.fr",
       imgP: hubsite,
       skillName: [html5, css3, js, jquery, bootstrap, vscode, Wordpress],
     },
     {
-      id: "2",
+      id: "3",
       projectName: "Hack In Paris",
       lienProject: "www.hackinparis.com",
       imgP: hip,
       skillName: [adobeXD, Wordpress],
     },
     {
-      id: "3",
+      id: "4",
       projectName: "SysDream",
       lienProject: "www.sysdream.com",
       imgP: sysdream,
